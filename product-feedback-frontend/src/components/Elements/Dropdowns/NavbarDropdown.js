9617c87b7ce660ff95d2149a074404c0770363ea
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-04-04 01:38:36 +0800

    changed the sort button a lil bit

--- product-feedback-frontend/src/components/Elements/Dropdowns/NavbarDropdown.js
+++ product-feedback-frontend/src/components/Elements/Dropdowns/NavbarDropdown.js
@@ -30,12 +30,64 @@ const NavbarDropdown = () => {
     sortArray(sortType);
   }, [sortType]);
 
+  /* onChange={(e) => dispatch(setFeedbackSortType(e.target.value))} */
+
   return (
     <>
-      <select onChange={(e) => dispatch(setFeedbackSortType(e.target.value))}>
-        <option value="mostUpvotes">Most Upvotes</option>
-        <option value="leastUpvotes">Least Upvotes</option>
-      </select>
+      <button
+        id="dropdownDefault"
+        data-dropdown-toggle="dropdown"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+      >
+        Dropdown button{" "}
+        <svg
+          class="ml-2 w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </button>
+      <div
+        id="dropdown"
+        class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+      >
+        <ul
+          class="py-1 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefault"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              onClick={() => {
+                dispatch(setFeedbackSortType("mostUpvotes"));
+              }}
+            >
+              Most Upvotes
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              onClick={() => {
+                dispatch(setFeedbackSortType("leastUpvotes"));
+              }}
+            >
+              Least Upvotes
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };

46328cfb7babee14f6a6d337bf4a82a84d67d140
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-04-30 02:49:30 +0800

    somewhat desktop responsive

--- product-feedback-frontend/src/components/Homepage/Homepage.js
+++ product-feedback-frontend/src/components/Homepage/Homepage.js
@@ -14,21 +14,35 @@ const Homepage = () => {
   };
 
   return (
-    <div className="container p-0 mx-auto sm:mt-14 md:px-9">
+    <div className="container p-0 mx-auto sm:mt-14 md:px-9 lg:px-40">
       {/* When screen is > 1024px (Desktop Screens) */}
+      <div className="hidden lg:contents">
+        <div className="flex flex-row gap-x-8">
+          <div className="flex flex-col gap-y-6">
+            <Hero handleSlideoverOpen={handleSlideoverOpen} open={open} />
+            <FeedbackCategory />
+            <FeedbackCategory />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <FeedbackNavbar />
+            <div className="border-2 border-solid">Feedback List</div>
+          </div>
+        </div>
+      </div>
 
       {/* When screen is > 768px (Tablet Screens) */}
 
-      <div className="hidden sm:contents md:flex md:flex-col">
+      <div className="hidden sm:contents lg:hidden">
         <div className="flex flex-row justify-between mb-10">
           <Hero handleSlideoverOpen={handleSlideoverOpen} open={open} />
-
           <FeedbackCategory />
           <FeedbackCategory />
         </div>
       </div>
 
       {/* When screen is < 640px (Mobile Screens) */}
+
       <div className="sm:hidden">
         <Hero handleSlideoverOpen={handleSlideoverOpen} open={open} />
       </div>
@@ -43,7 +57,7 @@ const Homepage = () => {
         <Slideover open={open} setOpen={handleSlideoverOpen} />
       </div>
 
-      <div className="">
+      <div className="lg:hidden">
         <FeedbackNavbar />
         <div className="border-2 border-solid">Feedback List</div>
       </div>
